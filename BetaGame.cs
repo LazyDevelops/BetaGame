dd71330346b05662d9be945bcc9898c920b651df
CheeseSand <choijoung1479@gmail.com>
2024-12-17 06:57:07 +0000

    feat: 이름 입력받는 기능 추가

--- BetaGame.cs
+++ BetaGame.cs
@@ -1,8 +1,58 @@
-﻿internal class BetaGame
+﻿using System.Text.RegularExpressions;
+
+internal class BetaGame
 {
     private static void Main()
-    {
-        VirtualTerminal.VirtualTerminal terminal = new("user");
+    {   
+        string? name = Input("이름을 입력해 주세요(영어, 숫자 만).\n> ",
+                             "다시 이름을 입력해 주세요(영어, 숫자 만).\n> ",
+                             @"^[a-zA-Z0-9]+$");
+
+        string check = "n";
+
+        while(check != "y" && check != "Y")
+        {
+            check = Input("이름이 \"" + name + "\" 맞습니까? (y/n)\n> ",
+                          "이름이 \"" + name + "\" 맞습니까? (y/n)\n> ",
+                          @"^[yYnN]$");
+
+            if(check == "n" || check == "N")
+            {
+                Console.Clear();
+                name = Input("이름을 입력해 주세요(영어, 숫자 만).\n> ",
+                             "다시 이름을 입력해 주세요(영어, 숫자 만).\n> ",
+                             @"^[a-zA-Z0-9]+$");
+            }
+        }
+
+        Console.Clear();
+        
+        VirtualTerminal.VirtualTerminal terminal = new(name);
         terminal.Run();
+    }
+
+    static string Input(string message, string retryMessage, string pattern){
+        Console.Write(message);
+
+        string? input = Console.ReadLine();
+
+        while (string.IsNullOrEmpty(input) || !CustomRegular(input, pattern))
+        {
+            Console.Clear();
+            Console.Write(retryMessage);
+            input = Console.ReadLine();
+        }
+
+        return input;
+    }
+
+    static bool CustomRegular(string? input, string pattern)
+    {
+        if (input == null)
+        {
+            return false;
+        }
+        
+        return Regex.IsMatch(input, pattern);
     }
 }
