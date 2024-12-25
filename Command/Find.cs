65516abc7d33386012f4e857b117e3e665451713
CheeseSand <choijoung1479@gmail.com>
2024-12-25 23:39:35 +0000

    chore: e

---
+++ Command/Find.cs
@@ -0,0 +1,24 @@
+﻿using VirtualTerminal.Error;
+using VirtualTerminal.FileSystem;
+using VirtualTerminal.Tree.General;
+
+namespace VirtualTerminal.Command
+{
+    public class FindCommand : VirtualTerminal.ICommand
+    {
+        public string? Execute(int argc, string[] argv, VirtualTerminal VT)
+        {
+            return null;
+        }
+
+        public string Description(bool detail)
+        {
+            if (detail)
+            {
+                return "find - ";
+            }
+
+            return "find - ";
+        }
+    }
+}
